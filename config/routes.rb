e54cc1393fa2e03fbaf2986a1c9c1279bec97050
muhammad-ahmad45 <muhammad.ahmad@xprolabs.com>
2023-09-19 19:43:15 +0500

    [102] add user table with email, name and password fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # root to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
