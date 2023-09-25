c71b959e43666e808021f1f83a562d3ba6d0feec
muhammad-ahmad45 <muhammad.ahmad@xprolabs.com>
2023-09-25 16:44:05 +0500

    Feedback Incorperated

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # DELETE /resource/sign_out
   def destroy
     super
   end
+
 end
