dac812f36565e34b4c2848ca68b78b8890760272
muhammad-ahmad45 <muhammad.ahmad@xprolabs.com>
2023-09-26 13:18:20 +0500

    Product: added controllers, views and edit,new,delete functionalty

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+
   root to: 'users#index'
   
-  # root to: "home#index" 
 
   devise_for :users, controllers: {
     session: 'users/sessions',
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   resources :users do
     resources :roles
     end
+
+  resources :products
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
